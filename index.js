23ae0f782e3a9925d4c3ea8217841a6489153673
eason112 <126652410+eason112@users.noreply.github.com>
2025-09-01 13:39:05 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const connectBTN = document.querySelector(".connect");
+const errorTxt = document.querySelector(".error");
+const dataTxt = document.querySelector(".data");
+const appUI = document.querySelector(".ui");
+const connectUI = document.querySelector(".connect-ui");
+
+let device;
+let dataChar;
+
+function handleRateChange(event) {
+    console.log(event.target.value)
+    dataTxt.textContent = parseHeartRate(event.target.value);
+}
+
+async function requestDevice() {
+  //only works for devices advertising heart rate service
+    const _options = { filters: [{ services: ["device_information"] }] };
+
+    const options = {
+        acceptAllDevices: true,
+        optionalServices: ["device_information"],
+    };
+    device = await navigator.bluetooth.requestDevice(options);
+    device.addEventListener("gattserverdisconnected", connectDevice);
+}
+
+async function connectDevice() {
+    if (device.gatt.connected) return;
+
+    const server = await device.gatt.connect();
+    const service = await server.getPrimaryService("device_information");
+
+    heartRate = await service.getCharacteristic("data_measurement");
+    heartRate.addEventListener("characteristicvaluechanged", handleRateChange);
+    console.log("connected");
+}
+
+async function init() {
+    if (!navigator.bluetooth) return errorTxt.classList.remove("hide");
+    if (!device) await requestDevice();
+
+    connectBTN.textContent = "connecting...";
+    await connectDevice();
+
+    appUI.classList.remove("hide");
+    connectUI.classList.add("hide");
+    //await startMonitoring();
+}
+
+
+
+connectBTN.addEventListener("click", init);
